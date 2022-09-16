58e2c0c8f31dcba93725b3a387d214fe198063c7
trbenedictpineda <benedict.pineda@tooltwist.com>
2022-09-16 10:55:18 +0800

    Updated the database config file.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 const { Sequelize } = require('sequelize');
+
+// Sample DB connection string
+// mysql://username:password@localhost:3306/dbname
 const db = new Sequelize(`${process.env.DATABASE}`)
 
 module.exports = db
